866dc3fdb45e57521aaee6a57f682aac329389f9
sachiv1999 <sachiv.chapadiya131951@marwadiuniversity.ac.in>
2025-07-01 16:03:46 +0530

    Delete routes directory

--- routes/cart.js
+++
@@ -1,8 +0,0 @@
-const express= require('express');
-const router= express.Router();
-const jwt = require('jsonwebtoken');
-const {userCart} = require('../controllers/cart.controller')
-
-router.get('/userCart',userCart);
-
-module.exports = router;
--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express=require('express');
-const router= express.Router();
-const userRoute = require('./user');
-const productRouter= require('./product');
-
-router.use('/user',userRoute);
-router.use('/product',productRouter);
-
-module.exports = router;
